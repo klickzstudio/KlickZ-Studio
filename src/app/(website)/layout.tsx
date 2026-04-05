eb5a0ff537c9ef99669ec99b3df31d3d11461f8a
Ainz <habeeburrahman386@gmail.com>
2026-04-05 23:27:43 +0530

    Edited UI & UX

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -63,7 +63,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${cormorant.variable} ${lato.variable} ${playfair.variable}`}>
+    <html lang="en" className={`${cormorant.variable} ${lato.variable} ${playfair.variable}`} data-scroll-behavior="smooth">
       <body>
         <JsonLd />
         <Navbar />
